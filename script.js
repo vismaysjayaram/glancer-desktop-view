2546d2e986bc23795805dcddd51f4264daaf2e07
Vismay S Jayaram <138998298+vismaysjayaram@users.noreply.github.com>
2026-07-04 21:14:28 +0400

    added blinking colon

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function updateClock(){
 	const now = new Date();
 	const hours = now.getHours();
 	const minutes = String(now.getMinutes()).padStart(2,"0");
-	document.querySelector('.time-card .clock-digits').textContent = `${hours}:${minutes}`
+	document.querySelector('.hours').textContent = hours;
+	document.querySelector('.minutes').textContent = minutes;
 
 	const options = {weekday: "long", year: "numeric", month: "long", day:"numeric"};
 	const dateStr = now.toLocaleDateString("en-US", options);
